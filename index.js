546f7aad84c2442b6f0e0c9b9b4134595a1f7162
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-04-07 16:20:54 -0300

    reconfigurando a lógica

--- index.js
+++ index.js
@@ -7,11 +7,32 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
-app.post('/', (req, res) => {
+app.post('/sicrede', (req, res) => {
 
     let watson = req.body.cnpj
     console.log("Informações do Bot: " + watson)
     msg = "Bot recebeu dados da API";
+
+    let arr = [
+        {
+            "ns": "BCKD20000700G",
+            "modelo": "Modelo DVR",
+            "data": "01102020",
+            "img": "https://imgur.com/9yYhjcA"
+        },
+        {
+            "ns": "BCKD20000700A",
+            "modelo": "Modelo Câmera IP",
+            "data": "01112021"
+        },
+        {
+            "ns": "BCKD20000700B",
+            "modelo": "Modelo NVD",
+            "data": "01042010"
+        }
+
+    ]
+
 
     if (tipo === "x") {
         msg2 = "você está conectado"
@@ -20,7 +41,7 @@ app.post('/', (req, res) => {
     }
 
     
-    return res.json(msg)
+    return res.json(arr)
 
 })
 
